938c39414c80383622018d1220e07998eb624fca
Dinakar potta <39981117+pheliosd@users.noreply.github.com>
2022-05-16 11:20:30 +0530

    Initial commit

---
+++ src/test/java/dbs/pheliosd/spring/basics/springin5steps/SpringIn5StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package dbs.pheliosd.spring.basics.springin5steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIn5StepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
